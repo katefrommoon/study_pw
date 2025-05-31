333c9640701db789bb419ad49cfec1d0b5893c53
Екатерина <katefrommoon@MacBook-Air-Ekaterina.local>
2025-05-31 19:48:36 +0300

    отчет allure и фикстуры

--- src/pages/main.page.js
+++ src/pages/main.page.js
@@ -1,12 +1,18 @@
+import { test } from "@playwright/test";
+
 export class MainPage {
-    constructor (page) {
-        this.page = page;
-        this.signupButton = page.getByRole('link', { name: 'Sign up' });
-    }
-    async open () {
-        await this.page.goto('https://realworld.qa.guru/');
-    }
-    async gotoLogin () {
-        await this.signupButton.click();
-    }
+  constructor(page) {
+    this.page = page;
+    this.signupButton = page.getByRole("link", { name: "Sign up" });
+  }
+  async open() {
+    return test.step("Открыть страницу", async () => {
+      await this.page.goto("https://realworld.qa.guru/");
+    });
+  }
+  async gotoLogin() {
+    return test.step("Кликнуть на кнопку", async () => {
+      await this.signupButton.click();
+    });
+  }
 }
